1de8a8fcb8c098ccc1ff0739010934d84dc07012
kinhcanhgioi <duyminhbk08@gmail.com>
2016-03-16 23:10:41 +0700

    live server

--- app/src/main/java/com/app/restfulapp/ultis/AppClientRequest.java
+++ app/src/main/java/com/app/restfulapp/ultis/AppClientRequest.java
@@ -11,15 +11,19 @@ public class AppClientRequest {
     private static AsyncHttpClient client = new AsyncHttpClient();
 
     public static void get(MainActivity mActivity,String url,  AsyncHttpResponseHandler responseHandler) {
-        client.setCookieStore(mActivity.getCookieStore());
-        client.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
+        initClient(mActivity);
         client.get(url, responseHandler);
     }
 
     public static void post(MainActivity mActivity,String url, AsyncHttpResponseHandler responseHandler) {
+        initClient(mActivity);
+        client.post(url, responseHandler);
+    }
+
+    private static void initClient(MainActivity mActivity) {
+//        client.setProxy();// define your proxy here
         client.setCookieStore(mActivity.getCookieStore());
         client.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36");
-        client.post(url, responseHandler);
     }
 
 }
